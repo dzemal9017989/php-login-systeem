223232c4fd8b0468e116246d6732695f4ec1f443
dzemal9017989 <114153831+dzemal9017989@users.noreply.github.com>
2023-06-13 15:05:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP login systeem</title>
+</head>
+<body>
+    <h1>PDO Login and Registration</h1><br>
+    <h1>Welcome op de HOME-pagina!</h1><br>
+    <h3>U bent niet ingelogd. Login in om verder te gaan.</h3><br>
+    <a href="login_form.php">Login</a>
+</body>
---
+++ registration.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP login systeem</title>
+</head>
+<body>
+    <h1>PHP - PDO Login and Registration</h1><br>
+    <h2>Het spel kan beginnen</h2><br>
+    <h4>Je bent ingelogd met:</h4><br>
+    <h4>Inlognaam: test</h4><br>
+    <h4>Password: test</h4><br>
+    <a href="index.php">Logout</a>
+</body>
+</html>
